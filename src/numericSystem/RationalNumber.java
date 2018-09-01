ec3fd28727fdeb22a7ece5f2110e405b27e7b312
iGoez <camilogoez2015@hotmail.com>
2018-08-31 22:20:30 -0500

    I fixed bugs and i did the substract and addRational methods

--- src/numericSystem/RationalNumber.java
+++ src/numericSystem/RationalNumber.java
@@ -9,19 +9,87 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 		this.numerator = numerator;
 		this.denominator = denominator;
 	}
+	
+	public int verifyNumberData(T n1, T n2) {
+		int value = 0;
+		
+		if (numerator instanceof Long || denominator instanceof Long) {
+			value = 1;
+		} else if (numerator instanceof Short || denominator instanceof Short) {
+			value = 2;
+		} else {
+			value = 3;
+		}
+		
+		return value;
+	}
 
 	@Override
 	public void addRational(RationalNumber<Number> rationalToBeAdded) {
-		// TODO Auto-generated method stub
-
+		if(denominator == rationalToBeAdded.getDenominator()) {
+			if(numerator instanceof Long) {
+				numerator = (T)Long.valueOf(numerator.longValue() + rationalToBeAdded.getNumerator().longValue());
+			}else if(numerator instanceof Short) {
+				numerator = (T)Short.valueOf((short) (numerator.shortValue() + rationalToBeAdded.getNumerator().shortValue()));
+			}else {
+			numerator = (T)Integer.valueOf(numerator.intValue() + rationalToBeAdded.getNumerator().intValue());
+			}
+		}else {
+			switch (verifyNumberData(numerator, denominator)) {
+			case 1:
+				numerator = (T)Long.valueOf(numerator.longValue()*rationalToBeAdded.getDenominator().longValue()
+											+denominator.longValue()*rationalToBeAdded.getNumerator().longValue());
+				denominator = (T)Long.valueOf(denominator.longValue()*rationalToBeAdded.getDenominator().longValue());
+				break;
+			case 2:
+				numerator = (T)Short.valueOf((short) (numerator.shortValue()*rationalToBeAdded.getDenominator().shortValue()
+													+ denominator.shortValue()*rationalToBeAdded.getNumerator().shortValue()));
+				denominator = (T)Short.valueOf((short)(denominator.shortValue()*rationalToBeAdded.getDenominator().shortValue()));
+				
+				break;
+			case 3:
+				numerator = (T)Integer.valueOf(numerator.intValue()*rationalToBeAdded.getDenominator().intValue()
+												+denominator.intValue()*rationalToBeAdded.getNumerator().intValue());
+				denominator = (T)Integer.valueOf(denominator.intValue()*rationalToBeAdded.getDenominator().intValue());
+				break;
+			}
+		}
+		simplifyRational();
 	}
 
 	@Override
 	public void substractRational(RationalNumber<Number> rationalToBeSubstracted) {
-		// TODO Auto-generated method stub
-
+		if(denominator == rationalToBeSubstracted.getDenominator()) {
+			if(numerator instanceof Long) {
+				numerator = (T)Long.valueOf(numerator.longValue() - rationalToBeSubstracted.getNumerator().longValue());
+			}else if(numerator instanceof Short) {
+				numerator = (T)Short.valueOf((short) (numerator.shortValue() - rationalToBeSubstracted.getNumerator().shortValue()));
+			}else {
+			numerator = (T)Integer.valueOf(numerator.intValue() - rationalToBeSubstracted.getNumerator().intValue());
+			}
+		}else {
+			switch (verifyNumberData(numerator, denominator)) {
+			case 1:
+				numerator = (T)Long.valueOf(numerator.longValue()*rationalToBeSubstracted.getDenominator().longValue()
+											-denominator.longValue()*rationalToBeSubstracted.getNumerator().longValue());
+				denominator = (T)Long.valueOf(denominator.longValue()*rationalToBeSubstracted.getDenominator().longValue());
+				break;
+			case 2:
+				numerator = (T)Short.valueOf((short) (numerator.shortValue()*rationalToBeSubstracted.getDenominator().shortValue()
+													- denominator.shortValue()*rationalToBeSubstracted.getNumerator().shortValue()));
+				denominator = (T)Short.valueOf((short)(denominator.shortValue()*rationalToBeSubstracted.getDenominator().shortValue()));
+				
+				break;
+			case 3:
+				numerator = (T)Integer.valueOf(numerator.intValue()*rationalToBeSubstracted.getDenominator().intValue()
+												-denominator.intValue()*rationalToBeSubstracted.getNumerator().intValue());
+				denominator = (T)Integer.valueOf(denominator.intValue()*rationalToBeSubstracted.getDenominator().intValue());
+				break;
+			}
+		}
+		simplifyRational();
 	}
-
+	
 	@Override
 	public void multiplyRational(RationalNumber<Number> rationalFactor) {
 		this.numerator = multiply(numerator, (T) rationalFactor.getNumerator());
@@ -32,11 +100,14 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 	private T multiply(T n1, T n2) {
 		T number = null;
 
-		if (n1 instanceof Long || n2 instanceof Long) {
+		switch (verifyNumberData(n1, n2)) {
+		case 1:
 			number = (T) Long.valueOf(n1.longValue() * n2.longValue());
-		} else if (n1 instanceof Short || n2 instanceof Short) {
+			break;
+		case 2:
 			number = (T) Short.valueOf((short) (n1.shortValue() * n2.shortValue()));
-		} else {
+			break;
+		case 3:
 			number = (T) Integer.valueOf(n1.intValue() * n2.intValue());
 		}
 		return number;
@@ -48,33 +119,24 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 		this.denominator = multiply(denominator, (T) rationalQuocient.getNumerator());
 		simplifyRational();
 	}
-	
-	private T divide(T n1, T n2) {
-		T number = null;
-
-		if (n1 instanceof Long || n2 instanceof Long) {
-			number = (T) Long.valueOf(n1.longValue() * n2.longValue());
-		} else if (n1 instanceof Short || n2 instanceof Short) {
-			number = (T) Short.valueOf((short) (n1.shortValue() * n2.shortValue()));
-		} else {
-			number = (T) Integer.valueOf(n1.intValue() * n2.intValue());
-		}
-		return number;
-	}
 
 	@Override
 	public void simplifyRational() {
 		T simplify = mcd();
 		
-		if (numerator instanceof Long || denominator instanceof Long) {
+		switch (verifyNumberData(numerator, denominator)) {
+		case 1:
 			numerator = (T)Long.valueOf(numerator.longValue() / simplify.longValue());
 			denominator = (T)Long.valueOf(denominator.longValue() / simplify.longValue());
-		} else if (numerator instanceof Short || denominator instanceof Short) {
+			break;
+		case 2:
 			numerator = (T)Short.valueOf((short) (numerator.shortValue() / simplify.shortValue()));
 			denominator = (T)Short.valueOf((short) (denominator.shortValue() / simplify.shortValue()));
-		} else {
+			break;
+		case 3:
 			numerator = (T)Integer.valueOf(numerator.intValue() / simplify.intValue());
 			denominator = (T)Integer.valueOf(denominator.intValue() / simplify.intValue());
+			break;
 		}
 	}
 
@@ -83,7 +145,8 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 		T d = null;
 		T aux = null;
 		
-		if (numerator instanceof Long || denominator instanceof Long) {
+		switch (verifyNumberData(numerator, denominator)) {
+		case 1:
 			u = (T) Long.valueOf(Math.abs(numerator.longValue()));
 			d = (T) Long.valueOf(Math.abs(denominator.longValue()));
 			while (d.longValue() != 0) {
@@ -91,7 +154,8 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 				u = d;
 				d = aux;
 			}
-		} else if (numerator instanceof Short || denominator instanceof Short) {
+			break;
+		case 2:
 			u = (T) Short.valueOf((short) Math.abs(numerator.shortValue()));
 			d = (T) Short.valueOf((short) Math.abs(denominator.shortValue()));
 			while (d.longValue() != 0) {
@@ -99,7 +163,8 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 				u = d;
 				d = aux;
 			}
-		} else {
+			break;
+		case 3:
 			u = (T) Integer.valueOf(Math.abs(numerator.intValue()));
 			d = (T) Integer.valueOf(Math.abs(denominator.intValue()));
 			while (d.longValue() != 0) {
@@ -107,6 +172,7 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 				u = d;
 				d = aux;
 			}
+			break;
 		}
 	
 		return u;
@@ -120,15 +186,20 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 
 	@Override
 	public void squaredRational() {
-		if (numerator instanceof Long || denominator instanceof Long) {
+		
+		switch (verifyNumberData(numerator, denominator)) {
+		case 1:
 			numerator = (T) Long.valueOf(numerator.longValue() * numerator.longValue());
 			denominator = (T) Long.valueOf(denominator.longValue() * denominator.longValue());
-		} else if (numerator instanceof Short || denominator instanceof Short) {
+			break;
+		case 2:
 			numerator = (T) Short.valueOf((short) (numerator.shortValue() * numerator.shortValue()));
 			denominator = (T) Short.valueOf((short) (denominator.shortValue() * denominator.shortValue()));
-		} else {
+			break;
+		case 3:
 			numerator = (T) Integer.valueOf(numerator.intValue() * numerator.intValue());
 			denominator = (T) Integer.valueOf(denominator.intValue() * denominator.intValue());
+			break;
 		}
 		simplifyRational();
 	}
@@ -144,5 +215,4 @@ public class RationalNumber<T extends Number> implements IRationalNumber<T> {
 		// TODO Auto-generated method stub
 		return this.denominator;
 	}
-
 }
